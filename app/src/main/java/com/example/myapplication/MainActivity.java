c2b9d7ba13fe9fb6b9cbcfc663e41e2eeac84e17
AashikaLilaramani <84186621+AashikaLilaramani@users.noreply.github.com>
2021-08-23 12:53:36 -0500

    new calc app! instructions on daily agenda

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void add(View v)
+    {
+        // Get references to all elements on app screen
+        EditText num1EditText = (EditText) findViewById(R.id.num1EditText1);
+    }
+
+}
