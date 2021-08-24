fb7b115b3c68ef8c7d401edb8ea17e43a85eab67
AashikaLilaramani <84186621+AashikaLilaramani@users.noreply.github.com>
2021-08-24 13:14:53 -0500

    pusj

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,69 @@ public class MainActivity extends AppCompatActivity {
     {
         // Get references to all elements on app screen
         EditText num1EditText = (EditText) findViewById(R.id.num1EditText1);
+        EditText num2EditText = (EditText) findViewById(R.id.num2EditText2);
+        TextView ansTextView = (TextView) findViewById(R.id.ansTextView);
+
+        //get the values from the EditText boxes and convert them to int data types
+        int num1 = Integer.parseInt(num1EditText.getText().toString());
+        int num2 = Integer.parseInt(num2EditText.getText().toString());
+
+        //add the two ints
+        int sum = num1 + num2;
+
+        //set the text for the answer
+        ansTextView.setText("Answer: " + sum);
+    }
+    public void subt(View v)
+    {
+        // Get references to all elements on app screen
+        EditText num1EditText = (EditText) findViewById(R.id.num1EditText1);
+        EditText num2EditText = (EditText) findViewById(R.id.num2EditText2);
+        TextView ansTextView = (TextView) findViewById(R.id.ansTextView);
+
+        //get the values from the EditText boxes and convert them to int data types
+        int num1 = Integer.parseInt(num1EditText.getText().toString());
+        int num2 = Integer.parseInt(num2EditText.getText().toString());
+
+        //subtract the two ints
+        int diff = num1 - num2;
+
+        //set the text for the answer
+        ansTextView.setText("Answer: " + diff);
+    }
+    public void div(View v)
+    {
+        // Get references to all elements on app screen
+        EditText num1EditText = (EditText) findViewById(R.id.num1EditText1);
+        EditText num2EditText = (EditText) findViewById(R.id.num2EditText2);
+        TextView ansTextView = (TextView) findViewById(R.id.ansTextView);
+
+        //get the values from the EditText boxes and convert them to int data types
+        int num1 = Integer.parseInt(num1EditText.getText().toString());
+        int num2 = Integer.parseInt(num2EditText.getText().toString());
+
+        //subtract the two ints
+        int div = num1/num2;
+
+        //set the text for the answer
+        ansTextView.setText("Answer: " + div);
+    }
+    public void mult(View v)
+    {
+        // Get references to all elements on app screen
+        EditText num1EditText = (EditText) findViewById(R.id.num1EditText1);
+        EditText num2EditText = (EditText) findViewById(R.id.num2EditText2);
+        TextView ansTextView = (TextView) findViewById(R.id.ansTextView);
+
+        //get the values from the EditText boxes and convert them to int data types
+        int num1 = Integer.parseInt(num1EditText.getText().toString());
+        int num2 = Integer.parseInt(num2EditText.getText().toString());
+
+        //subtract the two ints
+        int mult = num1*num2;
+
+        //set the text for the answer
+        ansTextView.setText("Answer: " + mult);
     }
 
 }
